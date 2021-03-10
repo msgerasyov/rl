48774956fac55f57306676ab1a0aba525ad4f264
msgerasyov <72410668+msgerasyov@users.noreply.github.com>
2021-03-10 19:49:57 +0300

    Update train_dqn.py

--- train_dqn.py
+++ train_dqn.py
@@ -45,10 +45,11 @@ if __name__ == '__main__':
 
     np.random.seed(seed)
     torch.manual_seed(seed)
-    if torch.cuda.is_available():
-        device  = torch.device('cuda:3')
-    else:
-        device = torch.device('cpu')
+    #if torch.cuda.is_available():
+        #device  = torch.device('cuda:3')
+    #else:
+        #device = torch.device('cpu')
+    device = torch.device('cpu')
     print(device)
     env = make_env(seed)
     state_shape = env.observation_space.shape
