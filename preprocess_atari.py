bdf34915e0ddb87dce13ff38a7fc752ee870d0a8
msgerasyov <72410668+msgerasyov@users.noreply.github.com>
2021-03-15 22:25:27 +0300

    Update preprocess_atari.py

--- preprocess_atari.py
+++ preprocess_atari.py
@@ -4,10 +4,10 @@ import torch
 import cv2
 
 import gym
-from gym.core import ObservationWrapper
+from gym.core import Wrapper
 from gym.spaces import Box
 
-class PreprocessAtari(ObservationWrapper):
+class PreprocessAtari(Wrapper):
     def __init__(self, env, height=42, width=42, color=False,
                  crop=lambda img: img, n_frames=4, dim_order='pytorch', reward_scale=1):
         """A gym wrapper that reshapes, crops and scales image into the desired shapes"""
@@ -63,7 +63,6 @@ class PreprocessAtari(ObservationWrapper):
             img = img.transpose([2, 0, 1])  # [h, w, c] to [c, h, w]
         return img
 
-
 class ClipRewardEnv(gym.RewardWrapper):
     def __init__(self, env):
         gym.RewardWrapper.__init__(self, env)
@@ -110,60 +109,16 @@ class EpisodicLifeEnv(gym.Wrapper):
         return obs
 
 
-class FrameBuffer(gym.Wrapper):
-    def __init__(self, env, n_frames=4, dim_order='tensorflow'):
-        """A gym wrapper that reshapes, crops and scales image into the desired shapes"""
-        super(FrameBuffer, self).__init__(env)
-        self.dim_order = dim_order
-        if dim_order == 'tensorflow':
-            height, width, n_channels = env.observation_space.shape
-            obs_shape = [height, width, n_channels * n_frames]
-        elif dim_order == 'pytorch':
-            n_channels, height, width = env.observation_space.shape
-            obs_shape = [n_channels * n_frames, height, width]
-        else:
-            raise ValueError(
-                'dim_order should be "tensorflow" or "pytorch", got {}'.format(dim_order))
-        self.observation_space = Box(0.0, 1.0, obs_shape)
-        self.framebuffer = np.zeros(obs_shape, 'float32')
-
-    def reset(self):
-        """resets breakout, returns initial frames"""
-        self.framebuffer = np.zeros_like(self.framebuffer)
-        self.update_buffer(self.env.reset())
-        return self.framebuffer
-
-    def step(self, action):
-        """plays breakout for 1 step, returns frame buffer"""
-        new_img, reward, done, info = self.env.step(action)
-        self.update_buffer(new_img)
-        return self.framebuffer, reward, done, info
-
-    def update_buffer(self, img):
-        if self.dim_order == 'tensorflow':
-            offset = self.env.observation_space.shape[-1]
-            axis = -1
-            cropped_framebuffer = self.framebuffer[:, :, :-offset]
-        elif self.dim_order == 'pytorch':
-            offset = self.env.observation_space.shape[0]
-            axis = 0
-            cropped_framebuffer = self.framebuffer[:-offset]
-        self.framebuffer = np.concatenate(
-            [img, cropped_framebuffer], axis=axis)
-
-
 def make_env(env_name, crop = lambda img: img, n_frames=1, clip_rewards=False, seed=None):
     env = gym.make(env_name)  # create raw env
     if seed is not None:
         env.seed(seed)
 
-    env = EpisodicLifeEnv(env)
+    #env = EpisodicLifeEnv(env)
 
     if clip_rewards:
         env = ClipRewardEnv(env)
 
     env = PreprocessAtari(env, height=80, width=80, crop=crop, n_frames=n_frames)
 
-    if n_frames > 1:
-        env = FrameBuffer(env, n_frames=n_frames, dim_order='pytorch')
     return env
