94107edb1a18807da7951fa35ff1d2253a8d49b9
msgerasyov <72410668+msgerasyov@users.noreply.github.com>
2021-03-15 22:24:57 +0300

    Update a3c.py

--- a3c.py
+++ a3c.py
@@ -9,80 +9,17 @@ os.environ["OMP_NUM_THREADS"] = "1"
 MAX_EP = 150000
 EVAL_FREQ = 150
 LSTM_SIZE = 128
+ENV_NAME = "KungFuMasterDeterministic-v0"
 
 import cv2
 import numpy as np
 from gym.core import Wrapper
 from gym.spaces.box import Box
 
-
-class PreprocessAtari(Wrapper):
-    def __init__(self, env, height=42, width=42, color=False,
-                 crop=lambda img: img, n_frames=4, dim_order='pytorch', reward_scale=1):
-        """A gym wrapper that reshapes, crops and scales image into the desired shapes"""
-        super(PreprocessAtari, self).__init__(env)
-        self.img_size = (height, width)
-        self.crop = crop
-        self.color = color
-        self.dim_order = dim_order
-
-        self.reward_scale = reward_scale
-        n_channels = (3 * n_frames) if color else n_frames
-
-        obs_shape = {
-            'theano': (n_channels, height, width),
-            'pytorch': (n_channels, height, width),
-            'tensorflow': (height, width, n_channels),
-        }[dim_order]
-
-        self.observation_space = Box(0.0, 1.0, obs_shape)
-        self.framebuffer = np.zeros(obs_shape, 'float32')
-
-    def reset(self):
-        """Resets the game, returns initial frames"""
-        self.framebuffer = np.zeros_like(self.framebuffer)
-        self.update_buffer(self.env.reset())
-        return self.framebuffer
-
-    def step(self, action):
-        """Plays the game for 1 step, returns frame buffer"""
-        new_img, r, done, info = self.env.step(action)
-        self.update_buffer(new_img)
-
-        return self.framebuffer, r * self.reward_scale, done, info
-
-    def update_buffer(self, img):
-        img = self.preproc_image(img)
-        offset = 3 if self.color else 1
-        if self.dim_order == 'tensorflow':
-            axis = -1
-            cropped_framebuffer = self.framebuffer[:, :, :-offset]
-        else:
-            axis = 0
-            cropped_framebuffer = self.framebuffer[:-offset, :, :]
-        self.framebuffer = np.concatenate([img, cropped_framebuffer], axis=axis)
-
-    def preproc_image(self, img):
-        """what happens to the observation"""
-        img = self.crop(img)
-        img = cv2.resize(img / 255, self.img_size, interpolation=cv2.INTER_LINEAR)
-        if not self.color:
-            img = img.mean(-1, keepdims=True)
-        if self.dim_order != 'tensorflow':
-            img = img.transpose([2, 0, 1])  # [h, w, c] to [c, h, w]
-        return img
-
-import gym
+from preprocess_atari import make_env
 
 def crop_func(img):
   return img[60:-30, 15:]
-
-def make_env():
-    env = gym.make("KungFuMasterDeterministic-v0")
-    env = PreprocessAtari(env, height=80, width=80,
-                          crop=crop_func,
-                          color=False, n_frames=1)
-    return env
 
 import torch
 import torch.nn as nn
@@ -263,7 +200,7 @@ class Worker(mp.Process):
       #self.g_ep, self.g_ep_r, self.res_queue = global_ep, global_ep_r, res_queue
       self.opt = opt
       self.master = master
-      self.env = make_env()
+      self.env = make_env(ENV_NAME, crop=crop_func)
       obs_shape = env.observation_space.shape
       n_actions = env.action_space.n
       self.lnet = AC_Net(obs_shape, n_actions, lstm_size=LSTM_SIZE)
@@ -323,7 +260,7 @@ class Worker(mp.Process):
         while iter < MAX_EP:
             self._sync_local_with_global()
             if iter % EVAL_FREQ == 0 and self.process_id == 0:
-                reward = np.mean(evaluate(self.master, make_env(), n_games=1))
+                reward = np.mean(evaluate(self.master, make_env(ENV_NAME, crop=crop_func), n_games=1))
                 torch.save(self.master.state_dict(), 'a3c.weights')
                 print(iter, reward)
             obs, actions, rewards, is_done, logits, state_values = self.work(20)
@@ -332,7 +269,7 @@ class Worker(mp.Process):
 
 if __name__ == "__main__":
 
-    env = make_env()
+    env = make_env(ENV_NAME, crop = crop_func)
     obs_shape = env.observation_space.shape
     n_actions = env.action_space.n
 
