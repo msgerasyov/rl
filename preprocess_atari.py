74a3f55edd2456fed64dc31b37e83da6e80339d8
msgerasyov <72410668+msgerasyov@users.noreply.github.com>
2021-03-21 23:15:29 +0300

    Update preprocess_atari.py

--- preprocess_atari.py
+++ preprocess_atari.py
@@ -4,74 +4,64 @@ import torch
 import cv2
 
 import gym
-from gym.core import ObservationWrapper, RewardWrapper
+from gym.core import Wrapper
 from gym.spaces import Box
 
-class PreprocessAtariObs(ObservationWrapper):
-    def __init__(self, env, crop, color=False):
-        """A gym wrapper that crops, scales image into the desired shapes and optionally grayscales it."""
-        ObservationWrapper.__init__(self, env)
-
-        self.color = color
+class PreprocessAtari(Wrapper):
+    def __init__(self, env, height=42, width=42, color=False,
+                 crop=lambda img: img, n_frames=4, dim_order='pytorch', reward_scale=1):
+        """A gym wrapper that reshapes, crops and scales image into the desired shapes"""
+        super(PreprocessAtari, self).__init__(env)
+        self.img_size = (height, width)
         self.crop = crop
-        self.img_size = (1, 80, 80)
-        self.observation_space = Box(0.0, 1.0, (1, 80, 80))
+        self.color = color
+        self.dim_order = dim_order
 
-    def _to_gray_scale(self, rgb_image, channel_weights=[0.8, 0.1, 0.1]):
-        img_gray = np.zeros(rgb_image.shape[:-1], dtype='float32')
-        for i in range(len(channel_weights)):
-          img_gray += channel_weights[i] * rgb_image[:,:,i]
-        return img_gray[np.newaxis,:,:]
-
-    def observation(self, img):
-        """what happens to each observation"""
-        img = self.crop(img)
-        img = cv2.resize(img, self.img_size[1:])
-        if not self.color:
-            img = img.mean(-1, keepdims=True)
-
-        return img.transpose([2, 0, 1]) / 255
-
-class RewardScaler(gym.RewardWrapper):
-    def __init__(self, env, reward_scale):
-        super().__init__(env)
         self.reward_scale = reward_scale
+        n_channels = (3 * n_frames) if color else n_frames
 
-    def reward(self, rew):
-        # modify rew
-        return rew * self.reward_scale
+        obs_shape = {
+            'theano': (n_channels, height, width),
+            'pytorch': (n_channels, height, width),
+            'tensorflow': (height, width, n_channels),
+        }[dim_order]
 
-class MaxAndSkipEnv(gym.Wrapper):
-    def __init__(self, env, skip=4):
-        """Return only every `skip`-th frame"""
-        gym.Wrapper.__init__(self, env)
-        # most recent raw observations (for max pooling across time steps)
-        self._obs_buffer = np.zeros(
-            (2,) + env.observation_space.shape, dtype=np.uint8)
-        self._skip = skip
+        self.observation_space = Box(0.0, 1.0, obs_shape)
+        self.framebuffer = np.zeros(obs_shape, 'float32')
+
+    def reset(self):
+        """Resets the game, returns initial frames"""
+        self.framebuffer = np.zeros_like(self.framebuffer)
+        self.update_buffer(self.env.reset())
+        return self.framebuffer
 
     def step(self, action):
-        """Repeat action, sum reward, and max over last observations."""
-        total_reward = 0.0
-        done = None
-        for i in range(self._skip):
-            obs, reward, done, info = self.env.step(action)
-            if i == self._skip - 2:
-                self._obs_buffer[0] = obs
-            if i == self._skip - 1:
-                self._obs_buffer[1] = obs
-            total_reward += reward
-            if done:
-                break
-        # Note that the observation on the done=True frame
-        # doesn't matter
-        max_frame = self._obs_buffer.max(axis=0)
+        """Plays the game for 1 step, returns frame buffer"""
+        new_img, r, done, info = self.env.step(action)
+        self.update_buffer(new_img)
 
-        return max_frame, total_reward, done, info
+        return self.framebuffer, r * self.reward_scale, done, info
 
-    def reset(self, **kwargs):
-        return self.env.reset(**kwargs)
+    def update_buffer(self, img):
+        img = self.preproc_image(img)
+        offset = 3 if self.color else 1
+        if self.dim_order == 'tensorflow':
+            axis = -1
+            cropped_framebuffer = self.framebuffer[:, :, :-offset]
+        else:
+            axis = 0
+            cropped_framebuffer = self.framebuffer[:-offset, :, :]
+        self.framebuffer = np.concatenate([img, cropped_framebuffer], axis=axis)
 
+    def preproc_image(self, img):
+        """what happens to the observation"""
+        img = self.crop(img)
+        img = cv2.resize(img / 255, self.img_size, interpolation=cv2.INTER_LINEAR)
+        if not self.color:
+            img = img.mean(-1, keepdims=True)
+        if self.dim_order != 'tensorflow':
+            img = img.transpose([2, 0, 1])  # [h, w, c] to [c, h, w]
+        return img
 
 class ClipRewardEnv(gym.RewardWrapper):
     def __init__(self, env):
@@ -80,51 +70,6 @@ class ClipRewardEnv(gym.RewardWrapper):
     def reward(self, reward):
         """Bin reward to {+1, 0, -1} by its sign."""
         return np.sign(reward)
-
-
-class FireResetEnv(gym.Wrapper):
-    def __init__(self, env):
-        """Take action on reset for environments that are fixed until firing."""
-        gym.Wrapper.__init__(self, env)
-        assert env.unwrapped.get_action_meanings()[1] == 'FIRE'
-        assert len(env.unwrapped.get_action_meanings()) >= 3
-
-    def reset(self, **kwargs):
-        self.env.reset(**kwargs)
-        obs, _, done, _ = self.env.step(1)
-        if done:
-            self.env.reset(**kwargs)
-        obs, _, done, _ = self.env.step(2)
-        if done:
-            self.env.reset(**kwargs)
-        return obs
-
-    def step(self, ac):
-        return self.env.step(ac)
-
-
-class EpisodicLifeEnv(gym.Wrapper):
-    def __init__(self, env):
-        """Make end-of-life == end-of-episode, but only reset on true game over.
-        Done by DeepMind for the DQN and co. since it helps value estimation.
-        """
-        gym.Wrapper.__init__(self, env)
-        self.lives = 0
-        self.was_real_done = True
-
-    def step(self, action):
-        obs, reward, done, info = self.env.step(action)
-        self.was_real_done = done
-        # check current lives, make loss of life terminal,
-        # then update lives to handle bonus lives
-        lives = self.env.unwrapped.ale.lives()
-        if lives < self.lives and lives > 0:
-            # for Qbert sometimes we stay in lives == 0 condition for a few frames
-            # so it's important to keep lives > 0, so that we only reset once
-            # the environment advertises done.
-            done = True
-        self.lives = lives
-        return obs, reward, done, info
 
     def reset(self, **kwargs):
         """Reset only when lives are exhausted.
@@ -139,72 +84,12 @@ class EpisodicLifeEnv(gym.Wrapper):
         self.lives = self.env.unwrapped.ale.lives()
         return obs
 
-
-# in torch imgs have shape [c, h, w] instead of common [h, w, c]
-class AntiTorchWrapper(gym.ObservationWrapper):
-    def __init__(self, env):
-        gym.ObservationWrapper.__init__(self, env)
-
-        self.img_size = [env.observation_space.shape[i]
-                         for i in [1, 2, 0]
-                         ]
-        self.observation_space = gym.spaces.Box(0.0, 1.0, self.img_size)
-
-    def observation(self, img):
-        """what happens to each observation"""
-        img = img.transpose(1, 2, 0)
-        return img
-
-
-class FrameBuffer(gym.Wrapper):
-    def __init__(self, env, n_frames=4, dim_order='tensorflow'):
-        """A gym wrapper that reshapes, crops and scales image into the desired shapes"""
-        super(FrameBuffer, self).__init__(env)
-        self.dim_order = dim_order
-        if dim_order == 'tensorflow':
-            height, width, n_channels = env.observation_space.shape
-            obs_shape = [height, width, n_channels * n_frames]
-        elif dim_order == 'pytorch':
-            n_channels, height, width = env.observation_space.shape
-            obs_shape = [n_channels * n_frames, height, width]
-        else:
-            raise ValueError(
-                'dim_order should be "tensorflow" or "pytorch", got {}'.format(dim_order))
-        self.observation_space = Box(0.0, 1.0, obs_shape)
-        self.framebuffer = np.zeros(obs_shape, 'float32')
-
-    def reset(self):
-        """resets breakout, returns initial frames"""
-        self.framebuffer = np.zeros_like(self.framebuffer)
-        self.update_buffer(self.env.reset())
-        return self.framebuffer
-
-    def step(self, action):
-        """plays breakout for 1 step, returns frame buffer"""
-        new_img, reward, done, info = self.env.step(action)
-        self.update_buffer(new_img)
-        return self.framebuffer, reward, done, info
-
-    def update_buffer(self, img):
-        if self.dim_order == 'tensorflow':
-            offset = self.env.observation_space.shape[-1]
-            axis = -1
-            cropped_framebuffer = self.framebuffer[:, :, :-offset]
-        elif self.dim_order == 'pytorch':
-            offset = self.env.observation_space.shape[0]
-            axis = 0
-            cropped_framebuffer = self.framebuffer[:-offset]
-        self.framebuffer = np.concatenate(
-            [img, cropped_framebuffer], axis=axis)
-
-
-
 def make_env(env_name, reward_scale, crop = lambda img: img, n_frames=1, seed=None):
     env = gym.make(env_name)  # create raw env
     if seed is not None:
         env.seed(seed)
-    env = EpisodicLifeEnv(env)
-    env = RewardScaler(env, reward_scale)
-    env = PreprocessAtariObs(env, crop)
+
+    env = PreprocessAtari(env, height=80, width=80, crop=crop,
+                            n_frames=n_frames, reward_scale=reward_scale)
 
     return env
