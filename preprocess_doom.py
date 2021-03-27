14d9dcde97131e240cefb9c00caaaa5fa10a32f7
msgerasyov <72410668+msgerasyov@users.noreply.github.com>
2021-03-27 16:34:35 +0300

    Update preprocess_doom.py

--- preprocess_doom.py
+++ preprocess_doom.py
@@ -13,14 +13,15 @@ def preprocess_frame(frame, crop):
     return img
 
 class DoomEnv():
-    def __init__(self, scenario, reward_scale, crop):
+    def __init__(self, cfg, reward_scale, crop):
         self.crop = crop
         self.observation_space = Box(0.0, 1.0, (1, 84, 84))
-        self.a_size = 3
-        self.actions = np.identity(self.a_size, dtype=bool).tolist()
         self.reward_scale = reward_scale
         self.game = DoomGame()
-        self.game.set_doom_scenario_path(scenario) 
+        self.game.load_config(cfg)
+        self.a_size = self.game.get_available_buttons_size()
+        self.actions = np.identity(self.a_size, dtype=bool).tolist()
+        #self.game.set_doom_scenario_path(scenario)
         self.game.set_doom_map("map01")
         self.game.set_screen_resolution(ScreenResolution.RES_160X120)
         self.game.set_screen_format(ScreenFormat.GRAY8)
@@ -29,17 +30,17 @@ class DoomEnv():
         self.game.set_render_weapon(True)
         self.game.set_render_decals(False)
         self.game.set_render_particles(False)
-        self.game.add_available_button(Button.MOVE_LEFT)
-        self.game.add_available_button(Button.MOVE_RIGHT)
-        self.game.add_available_button(Button.ATTACK)
+        #self.game.add_available_button(Button.MOVE_LEFT)
+        #self.game.add_available_button(Button.MOVE_RIGHT)
+        #self.game.add_available_button(Button.ATTACK)
         self.game.add_available_game_variable(GameVariable.AMMO2)
-        self.game.add_available_game_variable(GameVariable.POSITION_X)
-        self.game.add_available_game_variable(GameVariable.POSITION_Y)
-        self.game.set_episode_timeout(300)
-        self.game.set_episode_start_time(10)
+        #self.game.add_available_game_variable(GameVariable.POSITION_X)
+        #self.game.add_available_game_variable(GameVariable.POSITION_Y)
+        #self.game.set_episode_timeout(300)
+        #self.game.set_episode_start_time(10)
         self.game.set_window_visible(False)
         self.game.set_sound_enabled(False)
-        self.game.set_living_reward(-1)
+        #self.game.set_living_reward(-1)
         self.game.set_mode(Mode.PLAYER)
         self.game.init()
 
@@ -56,5 +57,5 @@ class DoomEnv():
         self.game.new_episode()
         return preprocess_frame(self.game.get_state().screen_buffer, self.crop)
 
-def make_env(scenario, reward_scale, crop = lambda img: img):
-    return DoomEnv(scenario, reward_scale, crop)
+def make_env(cfg, reward_scale, crop = lambda img: img):
+    return DoomEnv(cfg, reward_scale, crop)
